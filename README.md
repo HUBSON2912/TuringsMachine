858be041faef3397e731ccb818e6c133465d9318
Hubert Słowikowski <hubert.slowikowski.2912@gmail.com>
2025-05-31 22:58:38 +0200

    dodawanie

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-JESTEŚMY LUDEM WYBRANYM! BOGACTWO DLA NAS, WŁADZA DLA BELIARA.
-
 Instrukcje do maszyny Turinga podajesz w pliku `instrukcje.txt`.<br/>
-Maszyna obsługuje cztery systemy: **binarny** (2), **oktalny** (8), **decymalny** (10) i **heksadecymalny** (16).
+Maszyna obsługuje dwa systemy: **oktalny** (8), **decymalny** (10)
 
 Zachowanie jest podobne do zachowania Brainfucka:<br/>
-Możesz wpisać lub wypisać liczbę w dowolnym systemie oraz przesuwać się na boki po "taśmie". <br/> W odróżnieniu od BF tutaj taśma imituje nieskończoną: będąc na taśmie w punkcie 0 mogę przesunąć się w lewo tworząc nowe komórki (komórki zawsze są indeksowane od 0).
+Możesz wpisać lub wypisać liczbę w dowolnym z obsługiwanych systemów, przesuwać się na boki po "taśmie" oraz dodawać zaznaczone liczby.<br/> W odróżnieniu od BF tutaj taśma imituje nieskończoną: będąc na taśmie w punkcie 0 mogę przesunąć się w lewo tworząc nowe komórki (komórki zawsze są indeksowane od 0).<br/>Jeśli przesuniesz się poza zakres, wybrane do dodawania pozycje też się przesuwają tak, by zaznaczały wciąż tę samą liczbę. 
 
 Możliwe instrukcje:
 * `, SYSTEM LICZBA`     -   w obecnym miejscu na taśmię wpisz liczbę zapisaną w systemie SYSTEM
 * `. SYSTEM`            -   wypisz liczbę z obecnej pozycji głowicy nad taśmą i zapisz ją w systemie SYSTEM
-* `.`                   -   wyświetl całą taśmę
+* `.`                   -   wyświetl całą taśmę w systemie ósemkowym
 * `> ILOŚĆ`             -   przesuń się o ILOŚĆ pól w prawo
 * `< ILOŚĆ`             -   przesuń się o ILOŚĆ pól w lewo
+* `!`                   -   wybierz pierwszy składnik sumy
+* `@`                   -   wybierz drugi składnik sumy
+* `#`                   -   wybierz pozycję w której ma być zapisana suma
+* `+`                   -   dodaj wybrane liczby i zapisz ją w wybranej pozycji. Jeśli nie wybrano wcześniej pozycji z użyciem instrukcji `!`, `@`, `#` wyświetla błąd.
 
 Przykładowe instrukcje:
 * `, d 13` - dodaj 13 w systemie dziesiętnym
 * `, h 1a` - dodaj 1a w systemie szesnastkowym (26 w dziesiętnym)
 * `> 5` - przesuń tasmę w prawo o 5 pól
 * `. b` - wyświetl liczbę, nad którą znajduje się głowica, w systemie binarnym
+* `+` - dodaj liczby
 
 System ***MUSI*** być jedną literą:
-* b - system binarny
 * o - system oktalny
 * d - system decymalny
-* h - system heksadecymalny
-* x - system podany podczas wczytywania liczby
--- main.py
+++ main.py
@@ -1,46 +1,39 @@
-def daj_liczbe_binarnie(liczbaDec):
-    liczbaBin = str(bin(liczbaDec))
-    return liczbaBin[2:]  # odetnij 0b
-
-def daj_liczbe_oktalnie(liczbaDec):
-    liczbaBin = str(oct(liczbaDec))
-    return liczbaBin[2:]  # odetnij 0o
-
-def daj_liczbe_haksadecymalnie(liczbaDec):
-    liczbaBin = str(hex(liczbaDec))
-    return liczbaBin[2:]  # odetnij 0x
+def zamien_znak_na_liczbe(system_litera):
+    if system_litera=='o':
+        return 8
+    elif system_litera=='d':
+        return 10
+    else:
+        return -1
 
 class Maszyna:
 
     # metody "prywatne"
-    def __zmien_system_litera_na_system_liczba(self, litera):
-        match litera:
-            case 'b':
-                return 2
-            case 'h':
-                return 16
-            case 'o':
-                return 8
-            case 'd':
-                return 10
-            case _:
-                return -1
-
     def __daj_pusta_lista_o_rozmiarze(self, rozmiar):
         res=[]
         while rozmiar>0:
             rozmiar-=1
-            res.append( {"wartosc": None, "system": None} )
+            res.append(None)
         return res
 
     # metody "publiczne"
     def __init__(self, plik, rozmiar_maksymalny=5, pozycja=0):
-        self.tasma=self.__daj_pusta_lista_o_rozmiarze(rozmiar_maksymalny)  # [{"wartosc": w_dec, "system": sys}]
+        self.tasma=self.__daj_pusta_lista_o_rozmiarze(rozmiar_maksymalny)
         self.pozycja=pozycja
         self.plik_instrukcje=plik
+        self.dodawanie_pozycja_1=-1
+        self.dodawanie_pozycja_2=-1
+        self.dodawanie_pozycja_wynik=-1
     
     def dlugoscTasmy(self):
         return len(self.tasma)
+
+    def dodawanie(self):
+        if self.dodawanie_pozycja_1==-1 or self.dodawanie_pozycja_2==-1 or self.dodawanie_pozycja_wynik==-1:
+            print("ERROR: Nie wybrano pozycji")
+            return
+        
+        self.tasma[self.dodawanie_pozycja_wynik]= self.tasma[self.dodawanie_pozycja_1]+self.tasma[self.dodawanie_pozycja_2]
 
     def przesun(self, strona, ilosc):
         nowa_pozycja=self.pozycja
@@ -52,6 +45,12 @@ class Maszyna:
         # imitacja nieskończoności taśmy
         if nowa_pozycja<0:
             self.tasma=self.__daj_pusta_lista_o_rozmiarze(abs(nowa_pozycja)) + self.tasma
+            if self.dodawanie_pozycja_1!=-1:
+                self.dodawanie_pozycja_1+=abs(nowa_pozycja)
+            if self.dodawanie_pozycja_2!=-1:
+                self.dodawanie_pozycja_2+=abs(nowa_pozycja)
+            if self.dodawanie_pozycja_wynik!=-1:
+                self.dodawanie_pozycja_wynik+=abs(nowa_pozycja)
             nowa_pozycja=0
         elif nowa_pozycja>=self.dlugoscTasmy():
             self.tasma=self.tasma + self.__daj_pusta_lista_o_rozmiarze(nowa_pozycja-self.dlugoscTasmy()+1)
@@ -60,37 +59,48 @@ class Maszyna:
         self.pozycja=nowa_pozycja
 
     def wczytajLiczbe(self, system_litera, liczba_w_tym_systemie):
-        system=self.__zmien_system_litera_na_system_liczba(system_litera)
-        if system==-1:  # jeśli śmieszek podał x do wczytania to dodaj NIC
+        # wybor systemu
+        system=zamien_znak_na_liczbe(system_litera)
+
+        if system==-1:  # jeśli niewłaściwy sytstem to NONE
             system=None
             liczba=None
         else:
             liczba=int(liczba_w_tym_systemie, system)
-        self.tasma[self.pozycja]= {"wartosc": liczba, "system": system}  # nadpisanie
+        self.tasma[self.pozycja]=liczba # nadpisanie
 
     def wyswietlLiczbe(self, system):  # system to system NA KTÓRY MA ZOSTAĆ zamieniona liczba
-        system=self.__zmien_system_litera_na_system_liczba(system)
+        system=zamien_znak_na_liczbe(system)
         
+        # jesli niewłaściwy system to nic nie rób
         if system == -1:
-            system=self.tasma[self.pozycja]["system"]
+            return
         
-        liczbaDec=self.tasma[self.pozycja]["wartosc"]
+        liczbaDec=self.tasma[self.pozycja]
         match system:
-            case 2:
-                print(daj_liczbe_binarnie(liczbaDec))
-            case 8:
-                print(daj_liczbe_oktalnie(liczbaDec))
             case 10:
                 print(liczbaDec)
-            case 16:
-                print(daj_liczbe_haksadecymalnie(liczbaDec))
+            case 8:
+                print(oct(liczbaDec)[2:])
+            
 
     def wyswietlTasme(self):
         print("TASMA:")
         indeks=0
         for element in self.tasma:
-            print(indeks, element, sep=": ")
+            if element==None:
+                print(indeks, None, sep=": ")
+            else:
+                print(indeks, oct(element)[2:], sep=": ")
             indeks+=1
+
+    def zaznacz_pozycje(self, ktory_czynnik):
+        if ktory_czynnik=='!':
+            self.dodawanie_pozycja_1=self.pozycja
+        elif ktory_czynnik=='@':
+            self.dodawanie_pozycja_2=self.pozycja
+        elif ktory_czynnik=='#':
+            self.dodawanie_pozycja_wynik=self.pozycja
 
     def wykonaj(self):
         wejscie=open(self.plik_instrukcje, "r")
@@ -102,6 +112,10 @@ class Maszyna:
             elif linia[0] == ',':
                 znak, system, liczba=linia.split()
                 self.wczytajLiczbe(system, liczba)
+            elif linia[0] == '!' or linia[0] == '@' or linia[0] == '#':
+                self.zaznacz_pozycje(linia[0])
+            elif linia[0] == '+':
+                self.dodawanie()
             elif linia[0] == '.':
                 if linia==".\n" or linia==".":
                     self.wyswietlTasme()
